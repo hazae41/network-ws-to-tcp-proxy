4b17c33595af5da7ebbaf3b5130cb936c13b0ba4
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-02-18 00:17:04 +0100

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+# Example Network WebSocket-to-TCP proxy
+
+## Protocol
+
+Connect to the proxy via WebSocket with the following HTTP headers
+- `x-hostname` -> TCP target hostname
+- `x-port` -> TCP target port
+
+The connection then consists of two duplexes
+- you send/receive bytes to talk with the TCP target
+- you send/receive strings to talk with the proxy via JSON-RPC
+
+### Price
+
+The price is 1 wei = 1 byte of communication
+- Your balance is withdrawn when you send bytes to the TCP target
+- Your balance is withdrawn when the TCP target sends you bytes
+
+**You MUST pay before talking with the TCP target**
+
+All connections are closed when your balance is negative
+
+So you must count how many bytes you sent/received and pay when your balance is low
+
+### JSON-RPC
+
+The proxy accepts the following JSON-RPC methods
+
+#### net_pay
+```tsx
+{
+  jsonrpc: "2.0",
+  id: 123,
+  method: "net_pay",
+  params: [string[]]
+}
+```
+
+Params contains the list of Network secrets as raw unprefixed base16 string of length 64
+
+e.g.
+
+```tsx
+[
+  "e353e28d6b6a21a8188ef68643e4b93d41bca5baa853965a6a0c9ab7427138b0",
+  "e4a66606a16f0ec836570e9450f6ca5b1178418d326644d4f961b742f929ac92",
+  "e9f98c34b1e0f065584b58677d3217a91964823324f787742663e6cd05396fa1",
+  "edb0fff61d454abc85cf73de1d81ff2b4871900c95b14358b6db2b468a887f37",
+  "f1110e2b673a5840df3be8bffb5069b112c331fc7607aa8f92c80838ffcb40b5",
+  "f19746763e166c9ab79e212d84034b6b82702d75c91ff3e35568c5258f2282b3",
+  "f1cb66451b5979e35489242e0cc8690b2a8b0e0df54ada3db3d90ecadbff39de",
+  "f4abc45b9965d2f99f2a23f2caaaf70ba52e81f525188468a8501c79ac73f8b6",
+  "f7b61b995ee315720b6d64110098e6da84a70af0649f400ee4ea9f655b00e06c",
+  "ff874cacad0cdde5a675b712d7d7fe230774d2e555e73989d601a879db384ab9"
+]
+```
+
+**Each payment MUST contains at maximum 10 secrets with a minimum total value of 16384 wei (2 ^ 14)**
+
+It will return your new balance as a decimal bigint string
+
+```tsx
+{
+  jsonrpc: "2.0",
+  id: 123,
+  result: "123456789123456789"
+}
+```
---
+++ mod.ts
@@ -0,0 +1,133 @@
+import { writeAll } from "https://deno.land/std@0.216.0/io/mod.ts";
+import { RpcErr, RpcInvalidParamsError, RpcInvalidRequestError, RpcMethodNotFoundError, RpcOk, RpcRequestInit } from "npm:@hazae41/jsonrpc";
+import { NetworkMixin, base16_decode_mixed, base16_encode_lower, initBundledOnce } from "npm:@hazae41/network-bundle";
+
+await initBundledOnce()
+
+const chainIdNumber = 1
+const contractZeroHex = "0xFf61BB11819455d58944A83e44b87E80CFC19eA2"
+const receiverZeroHex = "0x39dfd20386F5d17eBa42763606B8c704FcDd1c1D"
+
+const secretBase16Set = new Set<string>()
+
+const chainIdBase16 = chainIdNumber.toString(16).padStart(64, "0")
+const chainIdMemory = base16_decode_mixed(chainIdBase16)
+
+const contractBase16 = contractZeroHex.slice(2).padStart(64, "0")
+const contractMemory = base16_decode_mixed(contractBase16)
+
+const receiverBase16 = receiverZeroHex.slice(2).padStart(64, "0")
+const receiverMemory = base16_decode_mixed(receiverBase16)
+
+const mixinStruct = new NetworkMixin(chainIdMemory, contractMemory, receiverMemory)
+
+async function onHttpRequest(request: Request) {
+  if (request.headers.get("upgrade") !== "websocket")
+    return new Response(undefined, { status: 400 })
+
+  const hostname = request.headers.get("x-hostname")
+  const port = request.headers.get("x-port")
+
+  if (!hostname)
+    return new Response(undefined, { status: 400 })
+  if (!port)
+    return new Response(undefined, { status: 400 })
+
+  const tcp = await Deno.connect({ hostname, port: Number(port) })
+
+  const { socket, response } = Deno.upgradeWebSocket(request)
+
+  socket.binaryType = "arraybuffer"
+
+  let balanceBigInt = 0n
+
+  const onForward = async (bytes: Uint8Array) => {
+    balanceBigInt -= BigInt(bytes.length)
+
+    if (balanceBigInt < 0n) {
+      socket.close()
+      tcp.close()
+      return
+    }
+
+    await writeAll(tcp, bytes)
+  }
+
+  const onBackward = (bytes: Uint8Array) => {
+    balanceBigInt += BigInt(bytes.length)
+
+    if (balanceBigInt < 0n) {
+      socket.close()
+      tcp.close()
+      return
+    }
+
+    socket.send(bytes)
+  }
+
+  const onMessage = (message: string) => {
+    const request = JSON.parse(message) as RpcRequestInit
+
+    if (request.method === "net_pay")
+      return onPayment(request)
+
+    socket.send(JSON.stringify(new RpcErr(request.id, new RpcMethodNotFoundError())))
+  }
+
+  const onPayment = (request: RpcRequestInit) => {
+    const [secretBase16Array] = request.params as [string[]]
+
+    if (secretBase16Array.length === 0) {
+      socket.send(JSON.stringify(new RpcErr(request.id, new RpcInvalidParamsError())))
+      return
+    }
+
+    if (secretBase16Array.length > 10) {
+      socket.send(JSON.stringify(new RpcErr(request.id, new RpcInvalidParamsError())))
+      return
+    }
+
+    let secretsBase16 = ""
+
+    for (const secretBase16 of secretBase16Array) {
+      if (secretBase16Set.has(secretBase16))
+        continue
+      secretBase16Set.add(secretBase16)
+      secretsBase16 += secretBase16
+    }
+
+    const secretsMemory = base16_decode_mixed(secretsBase16)
+
+    const totalMemory = mixinStruct.verify_secrets(secretsMemory)
+    const totalBase16 = base16_encode_lower(totalMemory)
+    const totalZeroHex = `0x${totalBase16}`
+    const totalBigInt = BigInt(totalZeroHex)
+
+    if (totalBigInt < 16_384n) {
+      socket.send(JSON.stringify(new RpcErr(request.id, new RpcInvalidRequestError())))
+      return
+    }
+
+    balanceBigInt += totalBigInt
+
+    console.log(totalBigInt, secretsBase16)
+
+    socket.send(JSON.stringify(new RpcOk(request.id, balanceBigInt.toString())))
+  }
+
+  tcp.readable
+    .pipeTo(new WritableStream({ write: onBackward }))
+    .catch(() => socket.close())
+
+  socket.addEventListener("message", async (event) => {
+    if (typeof event.data === "string")
+      return onMessage(event.data)
+    return await onForward(new Uint8Array(event.data))
+  })
+
+  socket.addEventListener("close", () => tcp.close())
+
+  return response
+}
+
+Deno.serve(onHttpRequest);
